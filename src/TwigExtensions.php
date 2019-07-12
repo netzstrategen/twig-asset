3af89d60a3d30c043317581c73519f93da87b8f1
Fabian Marz <fabian@netzstrategen.com>
2019-07-12 11:15:34 +0200

    Added asset function.

--- src/TwigExtensions.php
+++ src/TwigExtensions.php
@@ -7,21 +7,51 @@
 
 namespace Cue\twig_extensions;
 
+use Twig_Environment;
 use Twig_Extension;
+use Twig_SimpleFunction;
+use Waiter\WaiterConfig;
+use Waiter\WaiterHelper;
 
 /**
  * @implements Twig_ExtensionInterface
  */
 class TwigExtensions extends Twig_Extension {
 
+  public function getFunctions() {
+    return [
+      new Twig_SimpleFunction('asset', [$this, 'getAssetPath'], [
+        'needs_environment' => TRUE,
+      ]),
+    ];
+  }
+
   /**
-   * Returns the extension name.
+   * Appends the filemtime to the given asset path to ensure cache invalidation.
+   *
+   * @param Twig_Environment $env
+   *   The current Twig environment.
+   * @param $asset_path
+   *   The given path to the asset.
+   * @param bool $is_versioned
+   *   If the asset should be versioned by appending the filemtime.
    *
    * @return string
-   *   The extension name.
    */
-  public function getName(): string {
-    return 'cue_twig_extensions';
+  public static function getAssetPath(Twig_Environment $env, $asset_path, $is_versioned = TRUE): string {
+    $globals = $env->getGlobals();
+    $context_publication = $globals['contextPublication'];
+    $template_path = realpath($context_publication['templateDir']);
+    $asset_abspath = $template_path . '/' . $asset_path;
+
+    if (is_dir($template_path) && is_file($asset_abspath) && $is_versioned) {
+      $query_string = http_build_query([
+        'v' => filemtime($asset_abspath),
+      ]);
+      $asset_path = $asset_path . '?' . $query_string;
+    }
+
+    return $asset_path;
   }
 
 }
